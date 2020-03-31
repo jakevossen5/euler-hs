b51da1d035e59637684d40049feee5c112abe71b
Jake Vossen <jake@vossen.dev>
2020-03-31 10:48:03 -0600

    more problems

--- euler-hs/src/Lib.hs
+++ euler-hs/src/Lib.hs
@@ -1,7 +1,27 @@
 module Lib
-    ( getNFibs, getUnlimitedFibs, isFactor, primeList, isPrime, lowerSq, isPalindrome, isIntPalindrome
+    ( getNFibs
+    , getUnlimitedFibs
+    , getNextFib
+    , isFactor
+    , primeList
+    , isPrime
+    , lowerSq
+    , isPalindrome
+    , isIntPalindrome
+    , getFactors
+    , numFactors
+    , sumOfDigits
+    , factorial
+    , lastN
     ) where
+import Data.Char
 
+-- https://stackoverflow.com/questions/17252851/how-do-i-take-the-last-n-elements-of-a-list
+lastN n xs = drop (length xs - n) xs
+
+factorial n = foldl (\acc x -> acc * x) 1 [1..n]
+
+sumOfDigits n = sum $ map (digitToInt) (show $ n)
 
 isPalindrome :: [Char] -> Bool
 isPalindrome x = x == (reverse x)
@@ -18,6 +38,27 @@ noDivs n ds = foldr (\d r -> d*d > n || (rem n d > 0 && r)) True ds
 primeList  = 2 : 3 : filter (`noDivs` tail primeList) [5,7..]
 
 isPrime n = n > 1 && noDivs n primeList
+
+numFactors :: Integer -> Int
+numFactors n = length $ getFactors n
+
+-- http://rosettacode.org/wiki/Factors_of_an_integer
+getFactors n =
+  ds ++
+  [ r
+  | (d, 0) <- [divMod n r] 
+  , r <-
+     r :
+     [ d
+     | d > r ] ] ++
+  reverse (map (n `div`) ds)
+  where
+    r = floor (sqrt (fromIntegral n))
+    ds =
+      [ i
+      | i <- [1 .. r - 1] 
+      , mod n i == 0 ]
+
 
 isFactor :: Integer -> Integer -> Bool
 isFactor x y = x `mod` y == 0
